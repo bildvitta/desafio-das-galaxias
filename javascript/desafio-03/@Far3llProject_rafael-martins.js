237dd323ef970eb6dc543d536d8cfaed3a635012
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2020-04-24 11:04:02 -0300

    merge

--- javascript/desafio-03/@Far3llProject_rafael-martins.js
+++ javascript/desafio-03/@Far3llProject_rafael-martins.js
@@ -1,29 +1,60 @@
-function surpersinha(aposta){
-  if(aposta < 6){
-    let alert = "escolha entre 6 e 15"
-    return alert
-  }
-  
-  let qtdaposta = new Array(Number(aposta));
-  //console.log(qtdaposta.length)
-  let j = 0
-  for(let i = 0; i < qtdaposta.length; i++)
-  {
-    let numerosorteio = Math.floor(Math.random() * 59 + 1)
-    
-     while(j < i)
-      {
-        if (numerosorteio == qtdaposta[j]) 
-        { 
-         qtdaposta[j] = Math.floor(Math.random() * 59 + 1)
-          j = 0 
-        }else{
-          j++
-        }
-      }
-    qtdaposta[j] = numerosorteio
-  }
- return qtdaposta.sort()
-}
 
+function surpersinha(aposta){
+2
+  if(aposta < 6 || aposta > 15){
+3
+    let alert = "escolha entre 6 e 15"
+4
+    return alert
+5
+  }
+6
+  
+7
+  let qtdaposta = new Array(Number(aposta));
+8
+  //console.log(qtdaposta.length)
+9
+  let j = 0
+10
+  for(let i = 0; i < qtdaposta.length; i++)
+11
+  {
+12
+    let numerosorteio = Math.floor(Math.random() * 59 + 1)
+13
+    
+14
+     while(j < i)
+15
+      {
+16
+        if (numerosorteio == qtdaposta[j]) 
+17
+        { 
+18
+         qtdaposta[j] = Math.floor(Math.random() * 59 + 1)
+19
+          j = 0 
+20
+        }else{
+21
+          j++
+22
+        }
+23
+      }
+24
+    qtdaposta[j] = numerosorteio
+25
+  }
+26
+ return qtdaposta.sort( (a,b) => a-b)
+27
+}
+28
+​
+29
 console.log(surpersinha("6"))
+30
+​
