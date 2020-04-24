aef48815dd8cb4839b4fdb6bb272f20b8406fc4f
Douglas Calora <douglascalora123@gmail.com>
2020-04-23 22:31:44 -0300

    changed length

--- javascript/desafio-04/@douglascalora_douglas-calora.js
+++ javascript/desafio-04/@douglascalora_douglas-calora.js
@@ -1,13 +1,13 @@
 
 function maioriaDosVotos (votos = []) {
-  return votos.find(voto => votos.join('').match(new RegExp(voto, 'g')).length > votos.join('').length / 2)
+  return votos.find(voto => votos.join('').match(new RegExp(voto, 'g')).length > votos.length / 2)
 }
 
-console.log(maioriaDosVotos(["A", "A", "B"]))
+console.log(maioriaDosVotos(["caio", "cairo", "caio", "caio", "aí"]))
 // retorna "A"
 
-console.log(maioriaDosVotos(["A", "A", "A", "B", "C", "A"]))
-// retorna "A"
+// console.log(maioriaDosVotos(["A", "A", "A", "B", "C", "A"]))
+// // retorna "A"
 
-console.log(maioriaDosVotos(["A", "B", "B", "A", "C", "C"]))
-// retorna null
+// console.log(maioriaDosVotos(["A", "B", "B", "A", "C", "C"]))
+// // retorna null
