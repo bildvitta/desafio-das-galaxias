a6a9b0a1b0b18868cb86601120de18ae82319f6d
Douglas Calora <douglascalora123@gmail.com>
2020-04-27 10:18:50 -0300

    retornando null quando não encontra

--- javascript/desafio-04/@douglascalora_douglas-calora.js
+++ javascript/desafio-04/@douglascalora_douglas-calora.js
@@ -1,9 +1,8 @@
 function maioriaDosVotos (values = []) {
-  return values.find(value => values.filter(value2 => value === value2).length > values.length / 2)
+  return values.find(value => values.filter(value2 => value === value2).length > values.length / 2) || null
 }
 
-// console.log(maioriaDosVotos(["A", "A", "A", "B", "C", "A"]))
-// // retorna "A"
+console.log(maioriaDosVotos(["A", "A", "A", "B", "C", "A"]))
 console.log(maioriaDosVotos(['mari', 'marina', 'mariana', 'mariana', 'mariana']) )
-// console.log(maioriaDosVotos(["A", "B", "B", "A", "C", "C"]))
+console.log(maioriaDosVotos(["A", "B", "B", "A", "C", "C"]))
 // // retorna null
