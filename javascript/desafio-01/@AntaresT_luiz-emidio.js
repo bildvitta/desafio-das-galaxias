c5a18b83ace1f68ceb2b85dfadd79c8fb23f507e
AntaresT <luizeduardovbe@gmail.com>
2021-04-19 16:24:35 -0300

    Desafio 1 - Luiz emidio

--- javascript/desafio-01/@AntaresT_luiz-emidio.js
+++ javascript/desafio-01/@AntaresT_luiz-emidio.js
@@ -2,7 +2,7 @@ function convertBinary(value) {
   value = value.toString()
   var decimal = 0
   for (let count = value.length - 1, i = 0; count >= 0; count--, i++){
-    decimal += value[count] * Math.pow(2, i);
+    decimal += value[count] * Math.pow(2, i); 
   }
   console.log(decimal);
 }
