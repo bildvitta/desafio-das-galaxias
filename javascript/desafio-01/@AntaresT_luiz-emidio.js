b7790d81bdcd70534b6627df417577fd7a07403d
AntaresT <luizeduardovbe@gmail.com>
2021-04-20 18:11:59 -0300

    Code correction

--- javascript/desafio-01/@AntaresT_luiz-emidio.js
+++ javascript/desafio-01/@AntaresT_luiz-emidio.js
@@ -1,12 +1,13 @@
-function convertBinary(value) {
+function convertBinarytoDecimal (value) {
   value = value.toString()
   var decimal = 0
   for (let count = value.length - 1, i = 0; count >= 0; count--, i++){
     decimal += value[count] * Math.pow(2, i); 
   }
-  console.log(decimal);
+  return decimal
 }
 
-convertBinary('111101')
-convertBinary(111101)
-
+console.log(
+  convertBinarytoDecimal('111101'),
+  convertBinarytoDecimal(111101)
+)
